34b927ad6e6ba48581d46b55816045a04c05e0dc
Glucti <vanjatg@gmail.com>
2025-08-23 22:27:04 +0200

    working emulator

--- chip8.c
+++ chip8.c
@@ -4,21 +4,13 @@
 #include <stdint.h>
 #include <string.h>
 #include "chip8.h"
+#include <time.h>
 #include "screen.h"
 
 
-// 00E0 --> 2 bytes 
-// 2F E0 -> 0000 002F  0000 00E0 -> 2FE0 0000 
-uint16_t chip8_fetch(chip8* c) {
-    uint16_t opcode = (c->memory[c->PC] << 8) | c->memory[c->PC + 1];
-
-    c->PC += 2;
-
-    return opcode;
-}
-
 void chip8_init(chip8* c) {
     memset(c, 0, sizeof(chip8));
+    memset(c->keypad, 0, sizeof c->keypad);
 
     c->PC = ENTRY;
     for (int i = 0; i < 80; i++) {
@@ -26,7 +18,6 @@ void chip8_init(chip8* c) {
     }
 }
 
-// uint8_t screen[32][64];
 void drawSprite(uint8_t x, uint8_t y, uint8_t n, chip8* c) {
     c->V[0xF] = 0; // set the collision flag to 0
     for (int row = 0; row < n; row++) {
@@ -65,131 +56,265 @@ instruction_t decode_opcode(uint16_t opcode) {
     return inst;
 }
 
-// a2e0 -> xxxx xxxx xxxx xxxx
-void chip8_decode(chip8* c, uint16_t opcode) {
+// opcode table
 
-    instruction_t i = decode_opcode(opcode);
-    switch(opcode & 0xF000) {
-        case 0x0000:
-            switch(opcode) {
-                case 0x00E0:
-                    memset(c->screen, 0, sizeof c->screen);
-                    printf("CLS\n");
-                    break;
-                case 0x00EE: // pop the last address from stack and set the PC to it
-                    printf("RET\n");
-                    c->SP--;
-                    c->PC = c->stack[c->SP];
-                    break;
-                default:
-                    printf("SYS add -> ignored\n");
-                    break;
-            }
-            break;
-        case 0x2000: {
-            c->stack[c->SP] = c->PC;
-            c->SP++;
-            c->PC = i.NNN;
-            break;
-        }
-        case 0x1000: {
-            c->PC = i.NNN;
-            break;
-        }
-        case 0x3000: {
-            if (c->V[i.X] == i.NN) {
-                c->PC += 2;
-            } 
+op_handler main_table[16] = {
+    op_0_family,  
+    op_1NNN,      
+    op_2NNN,      
+    op_3XNN,      
+    op_4XNN,      
+    op_5XY0,      
+    op_6XNN,      
+    op_7XNN,      
+    op_8_family,  
+    op_9XY0,      
+    op_ANNN,      
+    op_BNNN,      
+    op_CXNN,      
+    op_DXYN,      
+    op_E_family,  
+    op_F_family   
+};
+
+typedef void (*op_handler)(chip8* c, instruction_t i);
+
+
+void op_0_family(chip8* c, instruction_t i) {
+    switch (i.NN) {
+        case 0xE0: {
+            memset(c->screen, 0, sizeof c->screen);
             break;
         }
-        case 0x4000: {
-            if (c->V[i.X] != i.NN) {
-                c->PC += 2;
-            }
-            break;
-        }
-        case 0x5000: {
-            if (c->V[i.X] == c->V[i.Y]) {
-                c->PC += 2;
-            }
-            break;
-        }
-        case 0x9000: {
-            if (c->V[i.X] != c->V[i.Y]) {
-                c->PC += 2;
-            }
-            break;
-        }
-        case 0x6000: {
-            c->V[i.X] = i.NN;
-            break;
-        }
-        case 0x7000: {
-            c->V[i.X] += i.NN;
-            break;
-        }
-        case 0x8000: {
-            switch(i.N) {
-                case 0: {
-                    c->V[i.X] = c->V[i.Y];
-                    break;
-                }
-                case 1: {
-                    c->V[i.X] |= c->V[i.Y];
-                    break;
-                }
-                case 2: {
-                    c->V[i.X] &= c->V[i.Y];
-                    break;
-                }
-                case 3: {
-                    c->V[i.X] ^= c->V[i.Y];
-                    break;
-                }
-                case 4: {
-                    uint16_t sum = c->V[i.X] + c->V[i.Y];
-                    c->V[0xF] = (sum > 0xFF);
-                    c->V[i.X] = (uint8_t)sum;
-                    break;
-                }
-                case 5: {
-                    c->V[0xF] = (c->V[i.X] >= c->V[i.Y]) ? 1 : 0;
-                    c->V[i.X] -= c->V[i.Y];
-                    break;
-                }
-                case 6: {
-                    c->V[i.X] = c->V[i.Y];
-                    c->V[0xF] = c->V[i.Y] & 0x1;
-                    c->V[i.X] >>= 1;
-                    break;
-                }
-                case 7: {
-                    c->V[0xF] = (c->V[i.Y] >= c->V[i.X]) ? 1 : 0;
-                    c->V[i.X] = c->V[i.Y] - c->V[i.X];
-                    break;
-                }
-                case 0xE: {
-                    c->V[i.X] = c->V[i.Y];
-                    c->V[0xF] = (c->V[i.Y] & 0x80) >> 7;
-                    c->V[i.X] <<= 1;
-                    break;
-                }
-            }
-            break;
-        }
-        case 0xA000: {
-            c->I = i.NNN;
-            break;
-        }
-        case 0XD000: {
-            drawSprite(i.X, i.Y, i.N, c);
+        case 0xEE: {
+            if (c->SP == 0) {printf("STACK UNDERFLOW\n"); return;} // DEBUG
+            c->SP--;
+            c->PC = c->stack[c->SP];
             break;
         }
         default:
-            printf("INVALID OPCODE\n");
             break;
     }
 }
+void op_1NNN(chip8* c, instruction_t i) {
+    c->PC = i.NNN;
+}
+void op_2NNN(chip8* c, instruction_t i) {
+    if (c->SP >= STACK_DEPTH) { printf("STACK OVERFLOW\n"); return;} // DEBUG
+    c->stack[c->SP] = c->PC;
+    c->SP++;
+    c->PC = i.NNN;
+}
+void op_3XNN(chip8* c, instruction_t i) {
+    if (c->V[i.X] == i.NN) 
+        c->PC += 2;
+}
+void op_4XNN(chip8* c, instruction_t i) {
+    if (c->V[i.X] != i.NN)
+        c->PC += 2;
+}
+void op_5XY0(chip8* c, instruction_t i) {
+    if ((i.opcode & 0x000f) != 0) {
+        return;
+    }
+    if (c->V[i.X] == c->V[i.Y]) {
+        c->PC += 2;
+    }
+}
+void op_6XNN(chip8* c, instruction_t i) {
+    c->V[i.X] = i.NN;
+}
+void op_7XNN(chip8* c, instruction_t i) {
+    c->V[i.X] += i.NN;
+}
+void op_8_family(chip8* c, instruction_t i) {
+    static op_handler table[16] = {
+        op_8XY0,
+        op_8XY1,
+        op_8XY2,
+        op_8XY3,
+        op_8XY4,
+        op_8XY5,
+        op_8XY6,
+        op_8XY7,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        op_8XYE,
+        NULL
+    };
+
+    op_handler fn = table[i.N];
+    if (fn) fn(c, i);
+    else printf("INVALID 8XY%X\n", i.N);
+}
+void op_8XY0(chip8* c, instruction_t i) {
+    c->V[i.X] = c->V[i.Y];
+}
+void op_8XY1(chip8* c, instruction_t i) {
+    c->V[i.X] |= c->V[i.Y];
+}
+void op_8XY2(chip8* c, instruction_t i) {
+    c->V[i.X] &= c->V[i.Y];
+}
+void op_8XY3(chip8* c, instruction_t i) {
+
+    c->V[i.X] ^= c->V[i.Y];
+}
+void op_8XY4(chip8* c, instruction_t i) {
+    uint16_t sum = c->V[i.X] + c->V[i.Y];
+    c->V[0xF] = (sum > 0xFF);
+    c->V[i.X] = (uint8_t)sum;
+}
+void op_8XY5(chip8* c, instruction_t i) {
+    c->V[0xF] = (c->V[i.X] >= c->V[i.Y]) ? 1 : 0;
+    c->V[i.X] -= c->V[i.Y];
+}
+void op_8XY6(chip8* c, instruction_t i) {
+    c->V[i.X] = c->V[i.Y];
+    c->V[0xF] = c->V[i.Y] & 0x1;
+    c->V[i.X] >>= 1;
+}
+void op_8XY7(chip8* c, instruction_t i) {
+    c->V[0xF] = (c->V[i.Y] >= c->V[i.X]) ? 1 : 0;
+    c->V[i.X] = c->V[i.Y] - c->V[i.X];
+}
+void op_8XYE(chip8* c, instruction_t i) {
+    c->V[i.X] = c->V[i.Y];
+    c->V[0xF] = (c->V[i.Y] & 0x80) >> 7;
+    c->V[i.X] <<= 1;
+}
+void op_9XY0(chip8* c, instruction_t i) {
+    if ((i.opcode & 0x000f) != 0) {
+        return;
+    }
+    if (c->V[i.X] != c->V[i.Y]) {
+        c->PC += 2;
+    }
+}
+void op_ANNN(chip8* c, instruction_t i) {
+    c->I = i.NNN;
+}
+void op_BNNN(chip8* c, instruction_t i) {
+    uint16_t target = i.NNN + c->V[0];
+    if (target < ENTRY || target >= MEM_SIZE) {
+        printf("BNNN to %03X out of range\n", target);
+    }
+}
+
+void op_CXNN(chip8* c, instruction_t i) {
+
+    uint8_t rnd = rand() & 0xFF;
+    c->V[i.X] = rnd & i.NN;
+}      
+void op_DXYN(chip8* c, instruction_t i) {
+    drawSprite(i.X, i.Y, i.N, c);
+}   
+void op_E_family(chip8* c, instruction_t i) {
+    switch (i.NN) {
+        case 0x9E: op_EX9E(c, i); break;
+        case 0xA1: op_EXA1(c, i); break;
+        default: printf("INVALID EX%02X\n", i.NN); break;
+    }
+}
+void op_EX9E(chip8* c, instruction_t i) {
+    printf("EXA1: V%X=%u keypad[%u]=%u\n", i.X, c->V[i.X], c->V[i.X] & 0xF, c->keypad[c->V[i.X] & 0xF]);
+    if (c->keypad[ c->V[i.X] & 0xF]) {
+        c->PC += 2;
+    }
+}
+
+void op_EXA1(chip8* c, instruction_t i) {
+    printf("EX9E: V%X=%u keypad[%u]=%u\n", i.X, c->V[i.X], c->V[i.X] & 0xF, c->keypad[c->V[i.X] & 0xF]);
+    if (!c->keypad[ c->V[i.X] & 0xF ]) {
+        c->PC += 2;
+    }
+}
+
+void op_F_family(chip8* c, instruction_t i) {
+    switch (i.NN) {
+        case 0x07: op_FX07(c, i); break;
+        case 0x15: op_FX15(c, i); break;
+        case 0x18: op_FX18(c, i); break;
+        case 0x1E: op_FX1E(c, i); break;
+        case 0x0A: op_FX0A(c, i); break;
+        case 0x29: op_FX29(c, i); break;
+        case 0x33: op_FX33(c, i); break;
+        case 0x55: op_FX55(c, i); break;
+        case 0x65: op_FX65(c, i); break;
+        default: printf("INVALID FX%02X\n", i.NN); break;
+    }
+}
+void op_FX07(chip8* c, instruction_t i) {
+    c->V[i.X] = c->DT;
+} 
+void op_FX15(chip8* c, instruction_t i) {
+    c->DT = c->V[i.X];
+}
+void op_FX18(chip8* c, instruction_t i) {
+    c->ST = c->V[i.X];
+}
+void op_FX1E(chip8* c, instruction_t i) {
+    c->I += c->V[i.X];
+}
+void op_FX0A(chip8* c, instruction_t i) {
+    for (uint8_t k =0; k < 16; k++) {
+        if (c->keypad[k]) {
+            c->V[i.X] = k;
+            return ;
+        }
+    }
+    c->PC -= 2;
+}
+void op_FX29(chip8* c, instruction_t i) {
+    c->I = c->V[i.X] * 5;
+}
+void op_FX33(chip8* c, instruction_t i) {
+    uint8_t val = c->V[i.X];
+    uint8_t hunnids = (val / 100) % 10;
+    uint8_t tens = (val / 10) % 10;
+    uint8_t ones = val % 10;
+
+    c->memory[c->I] = hunnids;
+    c->memory[c->I + 1] = tens;
+    c->memory[c->I + 2] = ones;
+}
+void op_FX55(chip8* c, instruction_t i) {
+    for (int r = 0; r <= i.X; r++) {
+        if (c->I + r < MEM_SIZE)
+            c->memory[c->I + r] = c->V[r];
+    }
+}
+void op_FX65(chip8* c, instruction_t i) {
+    for (int r = 0; r <= i.X; r++) {
+        if (c->I + r < MEM_SIZE) {
+            c->V[r] = c->memory[c->I + r];
+        }
+    }
+}
+
+
+
+// 00E0 --> 2 bytes 
+// 2F E0 -> 0000 002F  0000 00E0 -> 2FE0 0000 
+void chip8_fetch_decode(chip8* c) {
+    uint16_t opcode = (c->memory[c->PC] << 8) | c->memory[c->PC + 1];
+
+    c->PC += 2;
+
+    instruction_t inst = decode_opcode(opcode);
+
+    op_handler fn = main_table[(opcode & 0xF000) >> 12];
+    if (fn) {
+        fn(c, inst);
+    } else {
+        printf("INVALID OPCODE %04X at PC=%03X\n", opcode, c->PC - 2);
+    }
+}
+
 
 int read_f_into_entry(const char* path, chip8* c) {
     FILE *fp = fopen(path, "rb");
@@ -214,38 +339,47 @@ int read_f_into_entry(const char* path, chip8* c) {
     c->PC = ENTRY;
     return 1;
 }
+void chip8_timers(chip8* c) {
+    if (c->DT > 0) {
+        c->DT--;
+    }
+    if (c->ST > 0) {
+        c->ST--;
+        // beep witih SDL audio
+    }
+}
 
 
 int main() {
   chip8 chip;
   chip8_init(&chip);
+  srand((unsigned)time(NULL));
 
-  // load fonts 
-  for (int i = 0; i < 80; i++) chip.memory[i] = sprites[i];
 
-  if (!read_f_into_entry("1-chip8-logo.ch8", &chip)) return 1;
+  if (!read_f_into_entry("petdog.ch8", &chip)) return 1;
 
   SDL_Init(SDL_INIT_VIDEO);
   SDL_Window* window = SDL_CreateWindow("CHIP8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH_SCREEN * SCR_SCALE, HEIGHT_SCREEN * SCR_SCALE, SDL_WINDOW_SHOWN);
-  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-  int running = 1;
+  bool running = true;
   SDL_Event e;
+  uint32_t last_timer = SDL_GetTicks();
 
   while (running) {
-      while (SDL_PollEvent(&e)) {
-          if (e.type == SDL_QUIT) {
-              running = 0;
-          }
-      }
+      handle_input(&chip, &running);
 
       for (int i = 0; i < 10; i++) {
-        uint16_t op = chip8_fetch(&chip);
-        chip8_decode(&chip, op);
+        chip8_fetch_decode(&chip);
+      }
+      uint32_t now = SDL_GetTicks();
+      if (now - last_timer >= 16) {
+        chip8_timers(&chip);
+        last_timer = now;
       }
 
       drawScreen(renderer, &chip);
-      SDL_Delay(16);
+      SDL_Delay(1);
   }
 
   SDL_DestroyRenderer(renderer);
--- screen.h
+++ screen.h
@@ -3,9 +3,86 @@
 
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include "chip8.h"
 
-void drawScreen(SDL_Renderer* renderer, chip8* c);
+
+void handle_input(chip8* c, bool* running) {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                *running = 0;
+                break;
+            
+            case SDL_KEYDOWN: {
+
+
+                if (event.key.repeat) break;
+
+                switch (event.key.keysym.sym) {
+                    case SDLK_ESCAPE:
+                        *running = 0;
+                        break;
+
+                    case SDLK_1: c->keypad[0x1] = true; break;
+                    case SDLK_2: c->keypad[0x2] = true; break;
+                    case SDLK_3: c->keypad[0x3] = true; break;
+                    case SDLK_4: c->keypad[0xC] = true; break;
+
+                    case SDLK_q: c->keypad[0x4] = true; break;
+                    case SDLK_w: c->keypad[0x5] = true; break;
+                    case SDLK_e: c->keypad[0x6] = true; break;
+                    case SDLK_r: c->keypad[0xD] = true; break;
+
+                    case SDLK_a: c->keypad[0x7] = true; break;
+                    case SDLK_s: c->keypad[0x8] = true; break;
+                    case SDLK_d: c->keypad[0x9] = true; break;
+                    case SDLK_f: c->keypad[0xE] = true; break;
+
+                    case SDLK_z: c->keypad[0xA] = true; break;
+                    case SDLK_x: c->keypad[0x0] = true; break;
+                    case SDLK_c: c->keypad[0xB] = true; break;
+                    case SDLK_v: c->keypad[0xF] = true; break;
+
+                    default: break;
+                }
+                break;
+            }
+            
+            case SDL_KEYUP: {
+                switch (event.key.keysym.sym) {
+
+                    case SDLK_1: c->keypad[0x1] = false; break;
+                    case SDLK_2: c->keypad[0x2] = false; break;
+                    case SDLK_3: c->keypad[0x3] = false; break;
+                    case SDLK_4: c->keypad[0xC] = false; break;
+
+                    case SDLK_q: c->keypad[0x4] = false; break;
+                    case SDLK_w: c->keypad[0x5] = false; break;
+                    case SDLK_e: c->keypad[0x6] = false; break;
+                    case SDLK_r: c->keypad[0xD] = false; break;
+
+                    case SDLK_a: c->keypad[0x7] = false; break;
+                    case SDLK_s: c->keypad[0x8] = false; break;
+                    case SDLK_d: c->keypad[0x9] = false; break;
+                    case SDLK_f: c->keypad[0xE] = false; break;
+
+                    case SDLK_z: c->keypad[0xA] = false; break;
+                    case SDLK_x: c->keypad[0x0] = false; break;
+                    case SDLK_c: c->keypad[0xB] = false; break;
+                    case SDLK_v: c->keypad[0xF] = false; break;
+
+                    default: break;
+                }
+                break;
+            }
+
+
+        }
+    }
+}
 
 
 void drawScreen(SDL_Renderer* renderer, chip8* c) {
